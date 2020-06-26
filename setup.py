c921b67478e3377559320cc77d05d20b8970f7e2
Ross Wolf <31489089+rw-access@users.noreply.github.com>
2020-06-25 22:37:12 -0600

    Add GitHub actions and update package

--- setup.py
+++ setup.py
@@ -11,19 +11,10 @@ with io.open('eztoml/__init__.py', 'rt', encoding='utf8') as f:
     __version__ = re.search(r'__version__ = "(.*?)"', f.read()).group(1)
 
 
-test_requires = [
-    "pytest~=3.8.2",
-    "flake8==2.5.1",
-    "pep257==0.7.0",
-    "flake8-pep257==1.0.5",
-]
-
-
 setup(
     name='eztoml',
     version=__version__,
     description='Easy TOML',
-    install_requires=install_requires,
     author='Ross Wolf',
     license='MIT',
     classifiers=[
@@ -45,9 +36,5 @@ setup(
         'Topic :: Scientific/Engineering :: Information Analysis',
     ],
     url='https://eztoml.readthedocs.io',
-    tests_require=test_requires,
-    extras_require={
-        'test': test_requires,
-    },
     packages=['eztoml'],
 )
